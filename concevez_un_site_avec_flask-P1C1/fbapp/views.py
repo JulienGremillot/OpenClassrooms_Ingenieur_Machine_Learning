c510b35621f3d1b83fd0bc4b8ba5c76df5638275
Julien Gremillot <julien.gremillot@delia.tech>
2021-10-22 18:35:52 +0200

    Envoi des infos opengraph

--- concevez_un_site_avec_flask-P1C1/fbapp/views.py
+++ concevez_un_site_avec_flask-P1C1/fbapp/views.py
@@ -11,18 +11,30 @@ from .utils import find_content
 @app.route('/')
 @app.route('/index/')
 def index():
-    description = """
-    Toi, tu sais comment utiliser la console ! Jamais à court d'idées
-    pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs
-    volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu
-    hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit
-    problème d'autorité ? ;-)
-    """
+    og_description = 'Faites le test ultime et découvrez qui vous êtes vraiment !'
+    if 'img' in request.args:
+
+        img = request.args.get('img')
+        og_img = url_for('static', filename=img, _external=True)
+        og_url = url_for('index', img=img, _external=True)
+    else:
+        description = """
+        Toi, tu sais comment utiliser la console ! Jamais à court d'idées
+        pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs
+        volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu
+        hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit
+        problème d'autorité ? ;-)
+        """
+        og_img = url_for('static', filename='tmp/sample.jpg', _external=True)
+        og_url = url_for('index', _external=True)
     return render_template('index.html',
                            user_name="Tom",
                            user_image=url_for('static', filename='img/profile.png'),
                            description=description,
-                           blur=True
+                           blur=True,
+                           og_url=og_url,
+                           og_img=og_img,
+                           og_description=og_description
                            )
 
 @app.route('/result/')
@@ -31,9 +43,16 @@ def result():
     description = find_content(gender).description
     user_name = request.args.get('first_name')
     uid = request.args.get('id')
+    img = 'tmp/sample.jpg'
+    og_img = url_for('static', filename=img, _external=True)
+    og_url = url_for('index', _external=True, img=img)
+    og_description = 'Faites le test ultime et découvrez qui vous êtes vraiment !'
     profile_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
     return render_template('result.html',
                            user_name=user_name,
                            user_image=profile_pic,
-                           description=description
+                           description=description,
+                           og_url=og_url,
+                           og_img=og_img,
+                           og_description=og_description
                            )

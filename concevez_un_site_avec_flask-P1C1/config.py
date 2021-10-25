f3bf44d23e2e588ce8d5911e5576eb3d41f57537
Julien Gremillot <julien.gremillot@delia.tech>
2021-10-25 16:26:51 +0200

    mise à jour config

--- concevez_un_site_avec_flask-P1C1/config.py
+++ concevez_un_site_avec_flask-P1C1/config.py
@@ -1,8 +1,14 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
-FB_APP_ID = <MY_FB_APP_ID>
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    # Nouvelle base de données pour les tests.
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app_test.db')
+    # id de l'app TEST
+    FB_APP_ID = <MY_FB_APP_ID>
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = <MY_FB_APP_ID>
 
 SECRET_KEY = <MY_SECRET_KEY>

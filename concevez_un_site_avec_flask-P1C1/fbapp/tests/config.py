9a09c2f3a4a77b323e56c2f28af35e10dcef8313
Julien Gremillot <julien.gremillot@delia.tech>
2021-10-25 16:21:26 +0200

    mise à jour config

--- concevez_un_site_avec_flask-P1C1/fbapp/tests/config.py
+++ concevez_un_site_avec_flask-P1C1/fbapp/tests/config.py
@@ -1,13 +1,16 @@
 import os
-SECRET_KEY = 'votre_nouvelle_cle_secrete'
+SECRET_KEY = '<MY_SECRET_KEY>'
 
-# Remplacez par l'id de l'app TEST que vous avez créée précédemment.
-FB_APP_ID = 12345678901234567890
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Nouvelle base de données pour les tests.
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app_test.db')
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    # Nouvelle base de données pour les tests.
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app_test.db')
+    # id de l'app TEST
+    FB_APP_ID = <MY_FB_APP_ID>
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = <MY_FB_APP_ID>
 
 # Active le debogueur
 DEBUG = True
@@ -19,7 +22,7 @@ LIVESERVER_TIMEOUT = 10
 FB_USER_NAME = "toto toto"
 FB_USER_PW = "toto123"
 FB_USER_EMAIL = "toto_pjznlqe_toto@tfbnw.net"
-FB_USER_ID = 100018814390853
+FB_USER_ID = <MY_FB_USER_ID>
 FB_USER_GENDER = 'female'
 
 SERVER_NAME = 'localhost:8943'
